fe5ddc0ecd4a605366c28058b838beed99bec683
Moritz Schlarb <moschlar@metalabs.de>
2013-02-26 14:10:27 +0100

    These values should of course be empty

--- sauce/bin/suexec.h
+++ sauce/bin/suexec.h
@@ -5,15 +5,15 @@
 
 // Set this to the user that is allowed to run suexec
 // (e.g. the web application user)
-#define SOURCE_USER "moschlar"
+#define SOURCE_USER
 
 // Set this to the user and group which suexec will drop
 // privileges to.
 // To ensure that the temporary directory created by the web application
 // is read- and writable for the target user, you must set permissions and
 // ownership accordingly within the web application
-#define TARGET_USER "tine"
-#define TARGET_GROUP "tine"
+#define TARGET_USER
+#define TARGET_GROUP
 
 // Set this to the amount of time in microseconds
 // to wait between the various signals
